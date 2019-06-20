d6e054aaca4af4ab2250f07c28b407438a5b3044
leonardo <leonardo@caffelli.com>
2019-06-20 14:25:46 -0500

    feat: built

---
+++ src/components/wave.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { css } from "@emotion/core"
+
+const Wave = () => {
+    const [waves , setWaves] = useState(0);
+    const label = `:) ${waves} ${waves === 1 ? 'wave' : 'waves'}`
+
+
+    return (
+        <button
+            css={css`
+                background: red;
+                border:none;
+                color: white;
+                font-size: 1.25rem;
+            `}
+            onClick={()=> setWaves(waves +1)}>{label}</button>
+    )
+}
+
+export default Wave;
